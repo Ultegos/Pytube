0f320ef42c23854e09a105e001d0692d0c224db7
Ultegos <121366525+Ultegos@users.noreply.github.com>
2023-01-21 05:01:15 +0000

    added default code

---
+++ VideoDownloader.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+
+def download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+download(link)
